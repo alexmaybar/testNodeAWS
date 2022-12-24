ae15292f59cf622491fa8de19a1b68ee662dee76
Alex Aybar <alexmaybar@gmail.com>
2022-12-24 14:06:52 -0600

    Fix Typo

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => {
   res.send("Welcome to the home page baby");
 });
 
-const port = process.env.poirt || 3000;
+const port = process.env.port || 3000;
 app.listen(port, () => {
   console.log("Hello There");
 });
